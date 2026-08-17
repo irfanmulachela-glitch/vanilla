b4bfe57c23e1bef6da4d17e647ac68e90fb2c0f5
irfanmulachela-glitch <irfanmulachela@gmail.com>
2026-08-17 19:20:22 +0700

    fix: adjust logo sizes - larger flower, smaller text

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -57,8 +57,8 @@ export function Navigation() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 lg:h-20">
             {/* Logo */}
-            <Link href="/" className="flex items-center gap-3">
-              <div className="w-10 h-10 lg:w-12 lg:h-12 relative">
+            <Link href="/" className="flex items-center gap-2">
+              <div className="w-12 h-12 lg:w-14 lg:h-14 relative">
                 <Image
                   src="/logo.png"
                   alt="La Vanilla Supplier"
@@ -66,7 +66,7 @@ export function Navigation() {
                   className="object-contain"
                 />
               </div>
-              <div className="hidden sm:block w-24 lg:w-32 relative h-8 lg:h-10">
+              <div className="hidden sm:block w-20 lg:w-24 relative h-5 lg:h-6">
                 <Image
                   src="/logo-text.png"
                   alt="La Vanilla Supplier"
